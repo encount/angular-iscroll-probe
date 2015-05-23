0320cf2dbe2f1f1540f398419a3ef011afe8ee5f
Martin Thorsen Ranang <mtr@ranang.org>
2015-05-23 23:53:09 +0200

    New build.

--- dist/lib/angular-iscroll.min.js
+++ dist/lib/angular-iscroll.min.js
@@ -1,5 +1,5 @@
 /**
- * @license angular-iscroll-probe v1.3.2, 2015-05-23T23:29:20+0200
+ * @license angular-iscroll-probe v1.3.3, 2015-05-23T23:52:40+0200
  * (c) 2015 Martin Thorsen Ranang <mtr@ranang.org>
  * License: MIT
  */
