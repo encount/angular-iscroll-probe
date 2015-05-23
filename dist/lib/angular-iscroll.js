00dea9dff4b9166e5e3110daba485d8084d37aa0
Martin Thorsen Ranang <mtr@ranang.org>
2015-05-23 23:07:43 +0200

    New build.

--- dist/lib/angular-iscroll.js
+++ dist/lib/angular-iscroll.js
@@ -1,5 +1,5 @@
 /**
- * @license angular-iscroll v1.3.1, 2015-05-23T23:05:12+0200
+ * @license angular-iscroll-probe v1.3.1, 2015-05-23T23:07:31+0200
  * (c) 2015 Martin Thorsen Ranang <mtr@ranang.org>
  * License: MIT
  */
